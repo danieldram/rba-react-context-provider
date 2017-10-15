ecff944cf914575c34a3870baa401091d62684b4
Daniel Ram <danieldram@gmail.com>
2017-10-14 20:14:53 -0700

    add react-redux, prepare for refactored demo

--- src/Mocked.js
+++ src/Mocked.js
@@ -1,8 +1,41 @@
 import React, {Component} from "react";
 import ReactDOM from  "react-dom";
+import { createStore } from "redux";
+import {connect, Provider } from "react-redux";
 
 import ContextProvider from "./ContextProvider";
 import Test from "./Test";
+
+
+let store = (state = {testContent:"null"}, action) => {
+    switch(action.type){
+        case "TEST_CONTENT":
+            return {testContent: action.data}
+        default: 
+            return state
+    }
+};
+
+
+window.RBA_react = {};
+window.RBA_react.stores = {};
+let createdStore = createStore(store);
+window.RBA_react.stores.testStore = createdStore;
+
+
+const mapStateToProps = (state, props) => {
+    return {
+        testContent: state.testContent
+    }
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        onClick: ()=> {
+            dispatch({type: 'TEST_CONTENT', data:"test content"});
+        }
+    }
+}
 
 
 export default class Mocked extends Component {
@@ -10,14 +43,17 @@ export default class Mocked extends Component {
     render(){
         return(
             <div>
-                <ContextProvider component={Test} utils={{}}  actions={{PageServices:{}}}/>
+                <ContextProvider component={Test} {...this.props}/>
             </div>)
     }
 }
 
+const MappedComponent = connect(mapStateToProps, mapDispatchToProps)(Mocked);
 const root = document.getElementById('root');
 
-ReactDOM.render(<Mocked />, root);
+
+
+ReactDOM.render(<Provider store={createdStore}><MappedComponent/></Provider>, root);
 
 
 
